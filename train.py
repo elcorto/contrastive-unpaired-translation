77cea5113f23e95412371932e589dc21d5fcdd80
Steve Schmerler <git@elcorto.com>
2020-10-10 22:08:45 +0200

    train.py: remove visualizer
    This script assumes that we run a visdom server, else it insists on
    starting one by itself, which is quite annoying.
    
    Also, the visualizer instance is responsible for writing the loss log
    file. We write that file independently of visualizer. Additionally, we
    remove it before each run instead of appending to it.

--- train.py
+++ train.py
@@ -1,9 +1,11 @@
 import time
+import os
+
 import torch
+
 from options.train_options import TrainOptions
 from data import create_dataset
 from models import create_model
-from util.visualizer import Visualizer
 
 
 if __name__ == '__main__':
@@ -14,18 +16,20 @@ if __name__ == '__main__':
     model = create_model(opt)      # create a model given opt.model and other options
     print('The number of training images = %d' % dataset_size)
 
-    visualizer = Visualizer(opt)   # create a visualizer that display/save images and plots
-    opt.visualizer = visualizer
     total_iters = 0                # the total number of training iterations
 
     optimize_time = 0.1
 
-    times = []
+    # See util/visualizer.py:print_current_losses() for how this file is used
+    # to be written
+    loss_log_fn = os.path.join(opt.checkpoints_dir, opt.name, 'loss_log.txt')
+    if os.path.exists(loss_log_fn):
+        os.remove(loss_log_fn)
+
     for epoch in range(opt.epoch_count, opt.n_epochs + opt.n_epochs_decay + 1):    # outer loop for different epochs; we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>
         epoch_start_time = time.time()  # timer for entire epoch
         iter_data_time = time.time()    # timer for data loading per iteration
         epoch_iter = 0                  # the number of training iterations in current epoch, reset to 0 every epoch
-        visualizer.reset()              # reset the visualizer: make sure it saves the results to HTML at least once every epoch
 
         dataset.set_epoch(epoch)
         for i, data in enumerate(dataset):  # inner loop within one epoch
@@ -49,16 +53,21 @@ if __name__ == '__main__':
                 torch.cuda.synchronize()
             optimize_time = (time.time() - optimize_start_time) / batch_size * 0.005 + 0.995 * optimize_time
 
-            if total_iters % opt.display_freq == 0:   # display images on visdom and save images to a HTML file
-                save_result = total_iters % opt.update_html_freq == 0
-                model.compute_visuals()
-                visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
-
-            if total_iters % opt.print_freq == 0:    # print training losses and save logging information to the disk
+            # print training losses and save logging information to the disk
+            if total_iters % opt.print_freq == 0:
                 losses = model.get_current_losses()
-                visualizer.print_current_losses(epoch, epoch_iter, losses, optimize_time, t_data)
-                if opt.display_id is None or opt.display_id > 0:
-                    visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, losses)
+                line_dct = dict(
+                    epoch=epoch,
+                    epoch_iter=epoch_iter,
+                    total_iter=total_iters,
+                    t_data=t_data,
+                    optimize_time=optimize_time,
+                )
+                line_dct.update(losses)
+                line = ' '.join(f"{k}={v:<8.3g}" for k,v in line_dct.items())
+                print(line)
+                with open(loss_log_fn, 'a') as fd:
+                    fd.write(line + '\n')
 
             if total_iters % opt.save_latest_freq == 0:   # cache our latest model every <save_latest_freq> iterations
                 print('saving the latest model (epoch %d, total_iters %d)' % (epoch, total_iters))
